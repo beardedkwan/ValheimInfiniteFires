e5a33d9c6ba179279cf0c073ef7228150002bedd
amar5 <amar5@AEGLOS>
2024-05-25 11:51:48 -0500

    Add project files.

---
+++ ValheimInfiniteFires/ValheimInfiniteFires.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace ValheimInfiniteFires
+{
+    public class PluginInfo
+    {
+        public const string Name = "ValheimInfiniteFires";
+        public const string Guid = "beardedkwan" + Name;
+        public const string Version = "1.0.0";
+    }
+
+    [BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
+    [BepInProcess("valheim.exe")]
+    public class ValheimInfiniteFires : BaseUnityPlugin
+    {
+        [HarmonyPatch(typeof(Fireplace), "UpdateFireplace")]
+        public static class Fireplace_Patch
+        {
+            static void Prefix(Fireplace __instance, ref ZNetView ___m_nview)
+            {
+                ___m_nview.GetZDO().Set("fuel", __instance.m_maxFuel);
+            }
+        }
+    }
+}
